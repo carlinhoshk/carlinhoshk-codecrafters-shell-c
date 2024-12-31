f7d77408d45b1c81435af9a7c04e6744f1d81a6d
carlinhoshk <carlosmdohk@gmail.com>
2024-12-31 18:05:39 -0300

    Feat: subindo modificacoes do command not found

--- app/main.c
+++ app/main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
+  char input[100];
+  fgets(input, 100, stdin);
   // Flush after every printf
   setbuf(stdout, NULL);
 
   // Uncomment this block to pass the first stage
   printf("$ ");
 
+  input[strlen(input) - 1] = '\0';
+  printf("%s: command not found\n", input);
   // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
+  // fgets(input, 100, stdin);
   return 0;
 }
