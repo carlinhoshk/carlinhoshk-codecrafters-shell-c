07de23c35fce4e8b5db8f2380b040ba7e11d360a
carlinhoshk <carlosmdohk@gmail.com>
2025-01-01 01:28:16 -0300

    Feat: builtin

--- app/main.c
+++ app/main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,16 +10,32 @@ int main() {
     printf("$ ");
     fflush(stdout);
 
-    fgets(input, 100, stdin);
+    if (fgets(input, sizeof(input), stdin) == NULL) {
+      perror("Erro ao ler entrada");
+      continue;
+    }
     input[strcspn(input, "\r\n")] = 0;
 
-    if (!strcmp(input, "exit 0")) {
+    if (strcmp(input, "exit 0") == 0) {
       return 0;
     }
-    if (strncmp(input, "echo", strlen("echo")) == 0) {
+
+    if (strncmp(input, "type ", 5) == 0) {
+      char *command = input + 5; // Pega o comando após "type "
+      if (strcmp(command, "echo") == 0 || strcmp(command, "exit") == 0 ||
+          strcmp(command, "type") == 0) {
+        printf("%s is a shell builtin\n", command);
+      } else {
+        printf("%s: not found\n", command);
+      }
+      continue;
+    }
+
+    if (strncmp(input, "echo ", 5) == 0) {
       printf("%s\n", input + 5);
       continue;
     }
+
     printf("%s: command not found\n", input);
   }
 }
